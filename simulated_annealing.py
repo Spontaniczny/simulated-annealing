1a307545cf1e0ce64e7271342f0d3207f3450299
Spontaniczny <k2002kuba@gmail.com>
2023-04-12 02:32:12 +0200

    Zad 1 ~80%

--- simulated_annealing.py
+++ simulated_annealing.py
@@ -1,16 +1,34 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from random import uniform
+from math import exp
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def simulated_annealing(temps, start_pos, cost_func, pos_func):
+    pos = start_pos
+    energy1 = cost_func(pos)
+    # remember best pos and its energy
+    ans = start_pos
+    ans_energy = energy1
+    for temp in temps:
+        for _ in range(10):
+            next_pos = pos_func(pos)
+            energy2 = cost_func(next_pos)
+            delta_energy = energy1 - energy2
+            # update best pos if needed
+            if energy2 < ans_energy:
+                ans = next_pos
+                ans_energy = energy2
+            if delta_energy > 0 or exp(delta_energy / temp) > uniform(0, 1):
+                pos = next_pos
+                energy1 = energy2
+    return ans, ans_energy
+    # return ans
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def generate_temp_tab(max_temp, min_temp, temp_func):
+    temp = max_temp
+    tab = []
+    while temp > min_temp:
+        tab.append(temp)
+        temp = temp_func(temp)
+    return tab
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
